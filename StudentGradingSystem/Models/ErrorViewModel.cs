dfbdde40e1cbc1cc21aad208b34ff5de3d1f49b1
sreeshmarajm <sreeshmaraj101@gmail.com>
2025-10-24 22:04:55 +0400

    Deleted folder <folder-name>

--- StudentGradingSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StudentGradingSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
